a34c47da5ea225a3b1532c639d23c9b81816142b
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-04-13 16:53:50 +0000

    aa

--- coguMSHP/settings/prod.py
+++ coguMSHP/settings/prod.py
@@ -69,8 +69,8 @@ ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-# EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "False"
-EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "True") == "True"
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "False") == "False"
+# EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "True") == "True"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
