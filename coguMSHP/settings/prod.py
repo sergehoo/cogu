21a5e29e09a481b4a52ce84d2f30654c269d02cb
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-04-14 10:10:24 +0000

    po

--- coguMSHP/settings/prod.py
+++ coguMSHP/settings/prod.py
@@ -62,27 +62,17 @@ LOGGING = {
 
 SLACK_ALERT_WEBHOOK = os.environ.get("SLACK_ALERT_WEBHOOK", default=None)
 
-# ACCOUNT_EMAIL_VERIFICATION = 'none'  # Pour tests uniquement
-# Nouvelle méthode d'authentification : autoriser login via username et email
+
 ACCOUNT_LOGIN_METHODS = {"username", "email"}
-
-# Champs requis à l’inscription (le * signifie "requis")
 ACCOUNT_SIGNUP_FIELDS = ["username*", "email*", "password1*", "password2*"]
-
-# Email obligatoire + vérification stricte
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True  # Auto-confirmation via clic lien
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 3  # Optionnel
-
-# Limitation des tentatives de connexion
 ACCOUNT_RATE_LIMITS = {
     "login_failed": "5/m",  # 5 tentatives par minute
     "signup": "10/h",       # (optionnel) limiter les inscriptions
 }
 
-# Divers (facultatif mais utile)
-ACCOUNT_USERNAME_REQUIRED = True
-ACCOUNT_EMAIL_REQUIRED = True
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
